805955d25195127c5b8e84277ee5658c4ef47d9c
Yao_LL <2451357430@qq.com>
2024-09-22 19:53:13 +0800

    fix bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -20,8 +20,6 @@ const whenExternalScripts = (items = []) =>
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://uishd666.github.io',
-  base: '/OpenLamm',
   output: 'static',
   integrations: [
     tailwind({
